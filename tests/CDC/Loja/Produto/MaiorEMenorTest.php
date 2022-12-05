90d146d47a12050e21473953ac5d8f507f736cfe
jgnogueira <josegmnogueira@hotmail.com>
2022-12-05 12:10:05 -0300

    Adicionando novos cenários de testes

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -23,4 +23,47 @@ class MaiorEMenorTest extends TestCase
         $this->assertEquals('Jogo de pratos', $maiorMenor->getMenor()->getNome());
         $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
     }
+
+    public function testOrdemCrescente()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto('Jogo de pratos', 70.00));
+        $carrinho->adiciona(new Produto('Liquidificador', 250.00));
+        $carrinho->adiciona(new Produto('Geladeira',      450.00));
+        
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals('Jogo de pratos', $maiorMenor->getMenor()->getNome());
+        $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
+    }
+
+    public function testOrdemVariada()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto('Liquidificador', 250.00));
+        $carrinho->adiciona(new Produto('Jogo de pratos', 70.00));
+        $carrinho->adiciona(new Produto('Geladeira',      450.00));
+        
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals('Jogo de pratos', $maiorMenor->getMenor()->getNome());
+        $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
+    }
+
+    public function testUnicoProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto('Jogo de pratos', 70.00));
+        
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals('Jogo de pratos', $maiorMenor->getMenor()->getNome());
+        $this->assertEquals('Jogo de pratos', $maiorMenor->getMaior()->getNome());
+    }
 }
